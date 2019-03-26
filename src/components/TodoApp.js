86bdabc53f2929440fc874f377e9d7ff2b6cf70d
Maxsuel Maccari <maxmaccari@hotmail.com>
2019-03-25 20:14:04 -0400

    Implement Footer

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -4,7 +4,7 @@ import TodoForm from './TodoForm'
 import TodoList from './TodoList'
 import Footer from './Footer'
 import { saveTodo, loadTodos, destroyTodo, updateTodo } from '../lib/service'
-
+import { filterTodos } from '../lib/utils'
 
 export default class TodoApp extends Component {
   constructor(props) {
@@ -79,9 +79,11 @@ export default class TodoApp extends Component {
               handleTodoSubmit={this.handleTodoSubmit} />
           </header>
           <section className="main">
-            <TodoList todos={this.state.todos}
-              handleDelete={this.handleDelete}
-              handleToggle={this.handleToggle} />
+            <Route path='/:filter?' render={({match}) =>
+              <TodoList todos={ filterTodos(match.params.filter, this.state.todos) }
+                handleDelete={this.handleDelete}
+                handleToggle={this.handleToggle} />
+            } />
           </section>
           <Footer remaining={remaining} />
         </div>
